ee711fd6c38588b1358044e20e66503ea3e25b54
PONZO Justin <justin.ponzo9@etu.univ-lorraine.fr>
2023-12-14 20:39:15 +0100

    responsive design + legion clone done

--- src/Controller/LegionCloneController.php
+++ src/Controller/LegionCloneController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Legion;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class LegionCloneController extends AbstractController
 {
     #[Route('/legion-clone', name: 'app_legion_clone')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $legions = $entityManager->getRepository(Legion::class)->findAll();
+
         return $this->render('legion_clone/index.html.twig', [
             'controller_name' => 'LegionCloneController',
+            "legions" => $legions
         ]);
     }
 }
