5c2bffbd0975b9ebe5456c890c26f1c469ca34f7
PONZO Justin <justin.ponzo9@etu.univ-lorraine.fr>
2023-12-14 21:22:51 +0100

    template sabres-laser done

--- src/Controller/SabreLaserController.php
+++ src/Controller/SabreLaserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Sabre;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class SabreLaserController extends AbstractController
 {
     #[Route('/sabre-laser', name: 'app_sabre_laser')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $sabres = $entityManager->getRepository(Sabre::class)->findAll();
+
         return $this->render('sabre_laser/index.html.twig', [
             'controller_name' => 'SabreLaserController',
+            "sabres" => $sabres
         ]);
     }
 }
