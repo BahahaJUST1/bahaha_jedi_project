35bacc52195186434aab767fc1672b7278df084a
PONZO Justin <justin.ponzo9@etu.univ-lorraine.fr>
2023-12-14 16:36:51 +0100

    5 last war displayed

--- src/Controller/GuerreController.php
+++ src/Controller/GuerreController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Guerre;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class GuerreController extends AbstractController
 {
     #[Route('/guerre', name: 'app_guerre')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $guerres = $entityManager->getRepository(Guerre::class)->findAll();
+
         return $this->render('guerre/index.html.twig', [
             'controller_name' => 'GuerreController',
+            "guerres" => $guerres
         ]);
     }
 }
