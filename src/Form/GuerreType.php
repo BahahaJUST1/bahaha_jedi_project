883312b1c6d1052ea2078bbadb6068521ee34bcb
PONZO Justin <justin.ponzo9@etu.univ-lorraine.fr>
2024-01-17 15:13:28 +0100

    form ajout guerre 🔴

--- src/Form/GuerreType.php
+++ src/Form/GuerreType.php
@@ -1,21 +1,28 @@
 <?php
 
 namespace App\Form;
-
+ 
 use App\Entity\Guerre;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+ 
 class GuerreType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('planete')
-        ;
+            ->add('planete', TextType::class, [
+                'label' => 'planet',
+            ])
+            ->add('save', SubmitType::class, [
+                'attr' => ['class' => 'save'],
+            ])
+            ;
     }
-
+    
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
