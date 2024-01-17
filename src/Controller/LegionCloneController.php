64ab0107670de4eba726a5a44a26d0bf0180f75b
PONZO Justin <justin.ponzo9@etu.univ-lorraine.fr>
2024-01-17 15:28:21 +0100

    form ajout legion 🔴

--- src/Controller/LegionCloneController.php
+++ src/Controller/LegionCloneController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Legion;
+use App\Form\LegionType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LegionCloneController extends AbstractController
@@ -24,6 +26,35 @@ class LegionCloneController extends AbstractController
         return $this->render('legion_clone/index.html.twig', [
             'controller_name' => 'LegionCloneController',
             "legions" => $legions
+        ]);
+    }
+
+
+    #[Route('/legion-clone/form')]
+    public function indexNoLocaleForm(): Response
+    {
+        return $this->redirectToRoute('form_legion', ['_locale' => 'en']);
+    }
+
+
+    #[Route('/{_locale<%app.supported_locales%>}/legion-clone/form', name: 'form_legion', methods: ['GET', 'POST'])]
+    public function creation(Request $request, EntityManagerInterface $em): Response
+    {
+        $legion = new Legion();
+        $form = $this->createForm(LegionType::class, $legion);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($legion);
+            $em->flush();
+    
+            $this->addFlash('success', 'Légion créée avec succès !');
+            return $this->redirectToRoute('app_legion_clone');
+        }
+    
+        return $this->render('legion_clone/form.html.twig', [
+            'legion' => $legion,
+            'form' => $form->createView()
         ]);
     }
 }
