608de0d5326965d272a292763c303062fb6a8b74
PONZO Justin <justin.ponzo9@etu.univ-lorraine.fr>
2024-01-18 08:49:28 +0100

    form ajout sabre🔴🔵

--- src/Controller/SabreLaserController.php
+++ src/Controller/SabreLaserController.php
@@ -3,17 +3,26 @@
 namespace App\Controller;
 
 use App\Entity\Sabre;
+use App\Form\SabreType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SabreLaserController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/sabre-laser')]
     public function indexNoLocale(): Response
     {
-        return $this->redirectToRoute('app_sabre-laser', ['_locale' => 'en']);
+        return $this->redirectToRoute('app_sabre_laser', ['_locale' => 'en']);
     }
 
     #[Route('/{_locale<%app.supported_locales%>}/sabre-laser', name: 'app_sabre_laser')]
@@ -24,6 +33,40 @@ class SabreLaserController extends AbstractController
         return $this->render('sabre_laser/index.html.twig', [
             'controller_name' => 'SabreLaserController',
             "sabres" => $sabres
+        ]);
+    }
+
+
+    #[Route('/sabre-laser/form')]
+    public function indexNoLocaleForm(): Response
+    {
+        return $this->redirectToRoute('form_sabre', ['_locale' => 'en']);
+    }
+
+    #[Route('/{_locale<%app.supported_locales%>}/sabre-laser/form', name: 'form_sabre', methods: ['GET', 'POST'])]
+    public function creation(Request $request): Response
+    {
+        $sabre = new Sabre();
+        $form = $this->createForm(SabreType::class, $sabre);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            foreach ($sabre->getProprietaire() as $proprio) {
+                $proprio->addSabre($sabre);
+                $this->entityManager->persist($proprio);
+            }
+            
+            $this->entityManager->persist($sabre);
+            $this->entityManager->flush();
+    
+            $this->addFlash('success', 'Sabre créé avec succès !');
+            return $this->redirectToRoute('app_sabre_laser');
+        }
+    
+        return $this->render('sabre_laser/form.html.twig', [
+            'sabre' => $sabre,
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Form/SabreType.php
+++ src/Form/SabreType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Sabre;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +16,17 @@ class SabreType extends AbstractType
         $builder
             ->add('couleur')
             ->add('bi_lame')
-            ->add('proprietaire')
+
+            ->add('proprietaire', EntityType::class, [
+                'class' => 'App\Entity\UtilisateurForce',
+                'choice_label' => 'nom',
+                'multiple' => false,
+                'expanded' => true,
+            ])
+
+            ->add('save', SubmitType::class, [
+                'attr' => ['class' => 'save'],
+            ])
         ;
     }
 
